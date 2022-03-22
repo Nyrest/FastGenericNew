865c0cee6775ead7d9fd074e4baa8617c3187b90
Nyerst <Nyerst@users.noreply.github.com>
2022-03-22 22:22:07 +0800

    Update tests

---
+++ FastGenericNew.Tests/Attributes/TestCaseGenericAttribute.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace FastGenericNew.Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public sealed class TestCaseSourceGenericAttribute : TestCaseAttribute, ITestBuilder
+    {
+        public TestCaseSourceGenericAttribute(string sourceName) : base(sourceName)
+        {
+            SourceName = sourceName;
+        }
+
+        public TestCaseSourceGenericAttribute(Type type, string sourceName) : base(sourceName)
+        {
+            SourceContainerType = type;
+            SourceName = sourceName;
+        }
+
+        public string SourceName { get; set; }
+        public Type? SourceContainerType { get; set; }
+
+        IEnumerable<TestMethod> ITestBuilder.BuildFrom(IMethodInfo method, Test? suite)
+        {
+            if (!method.IsGenericMethodDefinition)
+                yield break;
+
+            var containerType = SourceContainerType ?? method.MethodInfo.DeclaringType as Type;
+            if (containerType is null)
+                yield break;
+
+            foreach (var member in containerType
+                .GetMembers(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static)
+                .Where(x => x.Name == SourceName)
+                )
+            {
+                IEnumerable? enumerable;
+                switch (member.MemberType)
+                {
+                    case MemberTypes.Field:
+                        enumerable = (IEnumerable?)((FieldInfo)member).GetValue(null);
+                        break;
+                    case MemberTypes.Method:
+                        enumerable = (IEnumerable?)((MethodInfo)member).Invoke(null, null);
+                        break;
+                    case MemberTypes.Property:
+                        enumerable = (IEnumerable?)((PropertyInfo)member).GetValue(null);
+                        break;
+                    default: continue;
+                }
+                if (enumerable is null) continue;
+
+                foreach (Type type in enumerable)
+                {
+                    var a = new TestMethod(method.MakeGenericMethod(type), suite);
+                    a.Name = $"{a.Name}<{type.FullName}>";
+                    yield return a;
+                }
+            }
+        }
+    }
+}
---
+++ FastGenericNew.Tests/Units/FastNewCoreTests/ExceptionsTest.cs
@@ -0,0 +1,74 @@
+﻿namespace FastGenericNew.Tests.Units.FastNewCoreTests;
+
+public class ExceptionsTest
+{
+    [Test()]
+    public void ExceptionInterface()
+    {
+        try
+        {
+            FastNew<IEnumerable>.CompiledDelegate();
+            Assert.Fail("The expected exception is not thrown.");
+        }
+        catch (MissingMethodException e)
+        {
+            Assert.IsTrue(e.Message.StartsWith("Cannot create an instance of an interface"));
+        }
+    }
+
+    [Test()]
+    public void ExceptionAbstract()
+    {
+        try
+        {
+            FastNew<Stream>.CompiledDelegate();
+            Assert.Fail("The expected exception is not thrown.");
+        }
+        catch (MissingMethodException e)
+        {
+            Assert.IsTrue(e.Message.StartsWith("Cannot create an abstract class"));
+        }
+    }
+
+    [Test()]
+    public void ExceptionPLString()
+    {
+        try
+        {
+            FastNew<string>.CompiledDelegate();
+            Assert.Fail("The expected exception is not thrown.");
+        }
+        catch (MissingMethodException e)
+        {
+            Assert.IsTrue(e.Message.StartsWith("No match constructor found in type"));
+        }
+    }
+
+    [Test()]
+    public void ExceptionNotFoundNoParameter()
+    {
+        try
+        {
+            FastNew<DemoClassNoParamlessCtor>.CompiledDelegate();
+            Assert.Fail("The expected exception is not thrown.");
+        }
+        catch (MissingMethodException e)
+        {
+            Assert.IsTrue(e.Message.StartsWith("No match constructor found in type"));
+        }
+    }
+
+    [Test()]
+    public void ExceptionNotFoundWithParameter()
+    {
+        try
+        {
+            FastNew<DemoClass, DBNull>.CompiledDelegate(DBNull.Value);
+            Assert.Fail("The expected exception is not thrown.");
+        }
+        catch (MissingMethodException e)
+        {
+            Assert.IsTrue(e.Message.StartsWith("No match constructor found in type"));
+        }
+    }
+}
