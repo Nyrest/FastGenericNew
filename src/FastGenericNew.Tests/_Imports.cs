1991800d8959b33c797d03a58b23994ba31d3270
Boring3 <16686147+Nyrest@users.noreply.github.com>
2024-08-14 17:58:49 +0800

    Upgrade to NUnit 4.x

--- src/FastGenericNew.Tests/_Imports.cs
+++ src/FastGenericNew.Tests/_Imports.cs
@@ -5,3 +5,9 @@ global using FastGenericNew;
 
 global using NUnit.Framework;
 global using FastGenericNew.Tests.Utilities;
+
+global using Assert = NUnit.Framework.Legacy.ClassicAssert;
+global using CollectionAssert = NUnit.Framework.Legacy.CollectionAssert;
+global using StringAssert = NUnit.Framework.Legacy.StringAssert;
+global using DirectoryAssert = NUnit.Framework.Legacy.DirectoryAssert;
+global using FileAssert = NUnit.Framework.Legacy.FileAssert;
